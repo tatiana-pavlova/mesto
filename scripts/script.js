71a5f09cd29641e55929f824146adbc4c8fab33c
Tatiana Pavlova <pavlova.tat.ole@gmail.com>
2021-05-20 22:46:11 +0300

    fix: fix review remarks

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,35 @@
-// открытие/закрытие поп-апа
 let openProfileEdit = document.querySelector('.profile__edit');
 let popup = document.querySelector('.popup');
 let closePopup = document.querySelector('.popup__close');
+let editName = document.querySelector('.popup__input_edit_name');
+let profileName = document.querySelector('.profile__name');
+let editJob = document.querySelector('.popup__input_edit_job');
+let profileJob = document.querySelector('.profile__job-info');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_edit_name');
+let jobInput = document.querySelector('.popup__input_edit_job');
 
-openProfileEdit.addEventListener('click', toggleClass);
 
-closePopup.addEventListener('click', toggleClass)
-
-function toggleClass() {
-  popup.classList.toggle('popup_opened');
+function openPopupForm() {
+  popup.classList.add('popup_opened');
+  editName.value = profileName.textContent;
+  editJob.value = profileJob.textContent;
 }
 
-// перенос данных из профайла в инпуты поп-апа
-let editName = document.querySelector('.popup__editName');
-let profileName = document.querySelector('.profile__name');
-let editJob = document.querySelector('.popup__editJob');
-let profileJob = document.querySelector('.profile__job-info');
-
-editName.value = profileName.textContent;
-editJob.value = profileJob.textContent;
-
-// внесение изменений в форму профиля
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__editName');
-let jobInput = document.querySelector('.popup__editJob');
+function closePopupForm() {
+  popup.classList.remove('popup_opened');
+}
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  toggleClass();
+  closePopupForm();
 }
+
+
+openProfileEdit.addEventListener('click', openPopupForm);
+
+closePopup.addEventListener('click', closePopupForm);
 
 formElement.addEventListener ('submit', formSubmitHandler);
